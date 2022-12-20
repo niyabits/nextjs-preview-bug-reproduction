a40fd1222c64785a084d91ee4d413aa212c0d9d2
Yash Gupta <yashguptaz@protonmail.com>
2022-12-20 15:33:37 +0530

    change base path & add redirection logic

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  basePath: "/docs",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/docs",
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
+};
