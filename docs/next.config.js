95017e146003ba5769d1a48079abca9b15e79d43
Yash Gupta <yashguptaz@protonmail.com>
2022-12-20 16:12:14 +0530

    try with svgrt

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,5 +1,31 @@
+const withSvgr = require("next-svgr");
+
 /** @type {import("next").NextConfig} */
-module.exports = {
+module.exports = withSvgr({
   reactStrictMode: true,
   basePath: "/docs",
-};
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx?/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "raw-loader",
+          options: {},
+        },
+      ],
+    });
+
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/docs",
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
+});
